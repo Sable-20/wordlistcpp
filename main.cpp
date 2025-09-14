eac456cc24ab2d8b9c66086b3b8e7c4e874ba60a
Sable Ayala <sable262021@gmail.com>
2025-09-14 17:05:14 -0400

    rework libraries

--- main.cpp
+++ main.cpp
@@ -2,23 +2,59 @@
 
 #include <cxxopts.hpp>
 
-#include "subcommands/fetch/fetch.hpp"
+#include "terminal_mods.hpp"
 
 #define VERSION "0.1.0"
 #define AUTHOR "Sable-20"
 
 int main(int argc, const char **argv) {
+    std::ostringstream os;
+    os  << "<subcommand> [OPTION...]\n\n"
+        << UNDERLINE << BOLD << "Available commands:" << RESET "\n\t"
+        << "fetch\n";
 
-   cxxopts::Options options = cxxopts::Options("wordlistcpp", "A C++ implementation of wordlistctl");
+    std::ostringstream description;
+    description << "A " << BOLD << RED << "C++" << RESET << " implementation of " << GREEN << "wordlistctl" << RESET;
+
+   cxxopts::Options options = cxxopts::Options("wordlistcpp", description.str())
+    .custom_help(os.str())
+    .positional_help("");
+
+    options
+        .set_width(80)
+        .allow_unrecognised_options()
+        .set_tab_expansion()
+        .add_options()
+            ("h,help", "Help find usage instructions on this program")
+            ("v,version", "Print the version information");
 
     // setup positional arguments
     options.add_options()
         ("command", "Which command to execute (i.e. fetch, search...)", cxxopts::value<std::string>());
 
-    setup_fetch(options);
+    options.add_options("Fetch")
+        ("l,list", "The wordlist to fetch", cxxopts::value<std::string>())
+        ("g,group", "group of wordlists to fetch", cxxopts::value<std::string>())
+        ("d,decompress", "Whether or not to decompress any compressed files", cxxopts::value<bool>())
+        ("w,workers", "Download workers", cxxopts::value<int>()->default_value("10"));
+
 
     options.parse_positional({"command"});
     auto result = options.parse(argc, argv);
+
+    if (argc < 2) {
+        std::cout << options.help() << std::endl;
+        return 0;
+    }
+
+    if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        exit(0);
+    }
+    if (result.count("version")) {
+        std::cout << "Version: " << VERSION << std::endl;
+        exit(0);
+    }
 
     return 0;
 }
---
+++ terminal_mods.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Sable Ayala on 9/14/25.
+//
+
+#ifndef WORDLISTCPP_TERMINAL_MODS_HPP
+#define WORDLISTCPP_TERMINAL_MODS_HPP
+
+#define UNDERLINE "\033[4m"
+#define RESET "\033[0m"
+#define BOLD "\033[1m"
+#define BLINK "\033[5m"
+
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+
+#endif //WORDLISTCPP_TERMINAL_MODS_HPP
