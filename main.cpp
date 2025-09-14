b20b4af06d1d276de97a1d1e15c8cfb97c0b229c
Sable Ayala <sable262021@gmail.com>
2025-09-14 11:21:48 -0400

    restart with cxxopts again

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-#include <memory>
 
-#include <CLI/CLI.hpp>
+#include "subcommands/fetch/fetch.hpp"
 
 #define VERSION "0.1.0"
 #define AUTHOR "Sable-20"
 
 int main(int argc, const char **argv) {
-    CLI::App app;
-
-
-    CLI11_PARSE(app, argc, argv);
 
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
--- subcommands/fetch/fetch.cpp
+++ subcommands/fetch/fetch.cpp
@@ -2,4 +2,4 @@
 // Created by Sable Ayala on 9/13/25.
 //
 
-#include "FETCH.hpp"
+#include "fetch.hpp"
